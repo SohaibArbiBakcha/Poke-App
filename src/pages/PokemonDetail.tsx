557ed84e6ecf56288ce04d1dc1172b5e8cf06e14
Sohaib Arbi BAKCHA <s.bakcha@atner.net>
2025-07-10 17:47:59 +0100

    remove loader

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -204,7 +204,6 @@ export const PokemonDetail: React.FC = () => {
   if (loading) {
     return (
       <div className="flex items-center justify-center min-h-screen">
-        <Loader2 className="w-8 h-8 animate-spin" />
         <img src="https://i.gifer.com/2iiJ.gif" alt="Loading" className="w-16 h-16 object-contain" />
         <span className="ml-2">{t('loading')}</span>
       </div>
